85c0c5a003745d41e5ee20603b0e96106bbe7e85
Jorge M. Carvalho <JorgeMiguelSCarvalho@gmail.com>
2020-01-31 23:20:43 +0000

    Added Part Scriptable Objects to Start Building Robots

---
+++ GGJ-20/Assets/Game Content/Scripts/Parts/Part.cs
@@ -0,0 +1,24 @@
+﻿using AssetIcons;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Robot Part that is used for assembling a robot.
+/// </summary>
+[CreateAssetMenu(fileName = "New Part", menuName = "Robot/Part")]
+public class Part : ScriptableObject
+{
+    [Header("Definition")]
+    [AssetIcon]
+    public Sprite sprite;
+    public GameObject instance;
+    public PartType partType;
+}
+
+public enum PartType
+{
+    Legs,
+    Body,
+    Head
+}
