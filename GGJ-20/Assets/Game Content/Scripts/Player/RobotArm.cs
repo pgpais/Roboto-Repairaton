3d371195626430ba162ed6834aa96f05d8b0e879
Jorge M. Carvalho <JorgeMiguelSCarvalho@gmail.com>
2020-02-01 01:19:45 +0000

    Started Arms Colliders. Added Size Clamp to Arms. Improved Sprite Rendering.

---
+++ GGJ-20/Assets/Game Content/Scripts/Player/RobotArm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class RobotArm : MonoBehaviour
+{
+    [Header("Rewired Info")]
+    public int playerId = 0; //set at Awake
+    
+    [Header("Input")]
+    public float armExtendSpeed = 5;
+    public float armRotationSpeed = 5;
+    public Vector2 armClampedSize = new Vector2(1, 14);
+
+    private Transform arm;
+    private Transform claw;
+    private Transform clawGear;
+    
+    private Player player;
+    private SpriteRenderer armRender;
+    private BoxCollider2D armCollider;
+    
+    private void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerId);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arm = transform.Find("Arm");
+        claw = arm.Find("Claw");
+        clawGear = claw.Find("ClawHead/ClawGear");
+
+        armRender = arm.GetComponent<SpriteRenderer>();
+        armCollider = arm.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        // Extend and shrink arm
+        float v = player.GetAxis("Vertical");
+        Vector2 size = armRender.size;
+        size.x = size.x + (v * armExtendSpeed * Time.deltaTime);
+        size.x = Mathf.Clamp(size.x, armClampedSize.x, armClampedSize.y);
+        armRender.size = size;
+
+        Vector2 clawPosition = claw.transform.localPosition;
+        clawPosition.x = size.x + 0.6f;
+        claw.transform.localPosition = clawPosition;
+
+        // Rotate Arms
+        float h = player.GetAxis("Horizontal");
+        Vector3 rot = transform.rotation.eulerAngles;
+        rot.z += h * armRotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(rot);
+
+        clawGear.Rotate(new Vector3(0, 0, h * armRotationSpeed * Time.deltaTime));
+
+        // Update Collider
+        Vector2 spriteSize = armRender.size;
+        armCollider.size = spriteSize;
+        armCollider.offset = new Vector2(spriteSize.x / 2, 0);
+
+        // Grab part
+    }
+}
