d050ddd52847a9a54f39475408bd78d332be328f
Cheese <pg.pais@campus.fct.unl.pt>
2020-02-01 21:53:53 +0000

    Lights have a flicker event now (needs GameManager)

---
+++ GGJ-20/Assets/Game Content/Scripts/Chaos Events/ChaosManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game_Content.Scripts.Chaos_Events
+{
+    public class ChaosManager : MonoBehaviour
+    {
+        public static UnityEvent LightFlicker = new UnityEvent();
+
+        public bool startEvent = false;
+
+        private void Update()
+        {
+            if(startEvent)
+                LightFlicker.Invoke();
+            startEvent = false;
+        }
+    }
+}
